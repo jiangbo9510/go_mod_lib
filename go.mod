22bad0015b56b8f35f36a53245dd874b9ad2a897
jiangbo9510 <jiangbo1234321@gmail.com>
2024-08-12 22:10:01 +0800

    new file

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jiangbo9510/go_mod_lib
+
+go 1.18
+
+replace github.com/jiangbo9510/test_base_lib => github.com/jiangbo9510/test_base_lib v0.0.3
---
+++ lib.go
@@ -0,0 +1,11 @@
+package go_mod_lib
+
+import "github.com/jiangbo9510/test_base_lib"
+
+func SetCommon(v string) {
+	test_base_lib.Common = v
+}
+
+func GetCommon() string {
+	return test_base_lib.GetCommon()
+}
