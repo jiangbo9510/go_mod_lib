e796a75fb0d64c7a0529a51ac3586b5f26976277
jiangbo9510 <jiangbo1234321@gmail.com>
2024-08-12 23:01:00 +0800

    update

--- lib.go
+++ lib.go
@@ -3,7 +3,7 @@ package go_mod_lib
 import "github.com/jiangbo9510/test_base_lib"
 
 func SetCommon(v string) {
-	test_base_lib.Common = v
+	test_base_lib.SetCommon(v, "")
 }
 
 func GetCommon() string {
